c0e1c7a874d21a8e1fb619774eaf9d8de28e9a28
spreet247 <samre2472@gmail.com>
2021-06-27 17:50:08 -0400

    logic

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function computerPlay() {
   const Random = Math.floor(Math.random() * things.length);
   return things[Random];
 }
+
+function game() {
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() !== computerSelection.toLowerCase()) {
+      return "You Lose! Paper beats Rock";
+    }
+  }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
